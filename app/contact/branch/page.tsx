8576ff20caad537fb8b67e4f96635be65eb812ca
smnahidweb <smnahidh040@gmail.com>
2026-04-22 19:26:43 +0600

    added basic routing

---
+++ app/contact/branch/page.tsx
@@ -0,0 +1,10 @@
+const ContactBranchPage = () => {
+  return (
+    <div>
+      <h1>Contact Branch</h1>
+      <p>Get in touch with our branch!</p>
+    </div>
+  );
+};
+
+export default ContactBranchPage;
