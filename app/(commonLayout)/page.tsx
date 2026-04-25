7546aa604ba83ef7460b1e872ed3845dbec939f0
smnahidweb <smnahidh040@gmail.com>
2026-04-25 21:04:00 +0600

    add a basic cookie

--- app/(commonLayout)/page.tsx
+++ app/(commonLayout)/page.tsx
@@ -1,12 +1,22 @@
 import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
 import { authClient } from "@/lib/auth-client";
+import { cookies } from "next/headers"
 
-export default async function Homsde() {
+export default async function Home() {
 
-  const session =  await authClient.getSession( );
-  console.log(session)
+const cookieStore = await cookies();
+console.log(cookieStore.toString())
 
+const result = await fetch("http://localhost:5000/api/auth/get-session",{
+
+headers:{
+   Cookie : cookieStore.toString()
+},
+cache :"no-store",
+
+})
+console.log(await result.json())
   return (
     <div >
      <p>Hello world</p>
